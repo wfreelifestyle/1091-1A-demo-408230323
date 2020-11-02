d45dc907faf2d4b43250a30c107d6850ce22de02
wfreelifestyle <408230323@gms.tku.edu.tw>
2020-11-03 01:15:49 +0800

    Initial commit, saving w01~w07 files

---
+++ w07/nav.js
@@ -0,0 +1,39 @@
+function change(index) {
+  //const p = document.getElementById('show');
+  const p = document.querySelector('#show');
+  console.log('p', p);
+  switch (index) {
+    case 1:
+      p.innerHTML = '<iframe width=100% height=100% src="./w01-intro_pokemon/intro.html">';
+
+      break;
+
+    case 2:
+      p.innerHTML = '<iframe width=100% height=100% src="./w01-intro_pokemon/pokemon.html">';
+      break;
+
+    case 3:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-imageGallery/imageGallery.html">';
+      break;
+
+    case 4:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-blog/blog.html">';
+      break;
+
+    case 5:
+      p.innerHTML = '<iframe width=100% height=100% src="./w03-div/div.html">';
+      break;
+
+    case 6:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w03-div-tku60/div_tku60.html">';
+      break;
+
+    case 7:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w06-ms-clone/index.html">';
+      break;
+  }
+}
